58010c307dd6b5f31a0eccc6449cc03e9c94446b
g.kharitonov <haritonovgleb123@gmail.com>
2025-03-29 13:38:40 +0300

    feat: sqlalchemy impl for repository

---
+++ components/open_gallery/persistence/repository.py
@@ -0,0 +1,30 @@
+from typing import override
+
+from sqlalchemy import Table, delete, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from open_gallery.shared.entity import EntityIdT, EntityT
+from open_gallery.shared.repository import Repository
+
+
+class SQLAlchemyRepository(Repository[EntityIdT, EntityT]):
+    def __init__(self, session: AsyncSession, table: Table, entity: type[EntityT]) -> None:
+        self._table = table
+        self._entity = entity
+        self._session = session
+
+    @override
+    async def get(self, entity_id: EntityIdT) -> EntityT | None:
+        stmt = select(self._entity).where(self._table.c.id == entity_id)
+        result = await self._session.execute(stmt)
+        return result.scalar()
+
+    @override
+    async def delete(self, entity_id: EntityIdT) -> None:
+        stmt = delete(self._table).where(self._table.c.id == entity_id)
+        await self._session.execute(stmt)
+
+    @override
+    async def save(self, entity: EntityT) -> EntityT:
+        self._session.add(entity)
+        return entity
