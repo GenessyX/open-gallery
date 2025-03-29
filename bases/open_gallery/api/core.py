4997d97b59094ad98f94f656f156e879712f4884
g.kharitonov <haritonovgleb123@gmail.com>
2025-03-30 01:34:17 +0300

    feat: enable custom types support

--- bases/open_gallery/api/core.py
+++ bases/open_gallery/api/core.py
@@ -3,10 +3,12 @@ import logging.config
 
 from fastapi import APIRouter, FastAPI
 
-from open_gallery.api.identity.router import identity_router
 from open_gallery.api.settings import APISettings
 from open_gallery.context.core import real_ip_ctx, request_id_ctx, sequence_ctx
 from open_gallery.logging.config import create_logging_config
+from open_gallery.shared_api.types import enable_types_support
+
+enable_types_support()
 
 logger = logging.getLogger(__name__)
 
@@ -28,6 +30,9 @@ def create_app(settings: APISettings | None = None) -> FastAPI:
     app = FastAPI(openapi_url=settings.server.openapi_url)
 
     api_v1 = APIRouter(prefix="/api/v1")
+
+    from open_gallery.api.identity.router import identity_router
+
     api_v1.include_router(identity_router)
 
     app.include_router(api_v1)
