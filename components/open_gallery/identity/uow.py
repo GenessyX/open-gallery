19046e33a43f2f2b6a356b82ea92925650924e61
g.kharitonov <haritonovgleb123@gmail.com>
2025-03-30 16:00:23 +0300

    fix: identity uow type

--- components/open_gallery/identity/uow.py
+++ components/open_gallery/identity/uow.py
@@ -1,10 +1,8 @@
 from abc import ABC
-from dataclasses import dataclass
 
 from open_gallery.identity.repository import UserRepository
 from open_gallery.shared.uow import UnitOfWork
 
 
-@dataclass(kw_only=True)
 class IdentityUnitOfWork(UnitOfWork, ABC):
     users: UserRepository
