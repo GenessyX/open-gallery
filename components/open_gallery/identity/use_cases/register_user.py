9bf97b781a0bcc8b0981dfd1dc59330b8db92563
g.kharitonov <haritonovgleb123@gmail.com>
2025-03-30 22:06:15 +0300

    feat(register): hash + verify password's complexity

--- components/open_gallery/identity/use_cases/register_user.py
+++ components/open_gallery/identity/use_cases/register_user.py
@@ -1,15 +1,24 @@
 from typing import override
 
+from open_gallery.hashing.interface import Hasher
 from open_gallery.identity.entities import User, UserRole
-from open_gallery.identity.exceptions import UserExistsError
+from open_gallery.identity.exceptions import UserExistsError, WeakPasswordError
 from open_gallery.identity.uow import IdentityUnitOfWork
+from open_gallery.passwords.complexity.interface import PasswordComplexityVerifier
 from open_gallery.shared.types import Email, SecretValue
 from open_gallery.shared.use_case import Usecase
 
 
 class RegisterUserUsecase(Usecase):
-    def __init__(self, uow: IdentityUnitOfWork) -> None:
+    def __init__(
+        self,
+        uow: IdentityUnitOfWork,
+        hasher: Hasher,
+        verifier: PasswordComplexityVerifier,
+    ) -> None:
         self._uow = uow
+        self._hasher = hasher
+        self._verifier = verifier
 
     @override
     async def __call__(self, email: Email, password: SecretValue[str]) -> User:
@@ -17,9 +26,14 @@ class RegisterUserUsecase(Usecase):
             existing = await uow.users.get_by_email(email=email)
             if existing:
                 raise UserExistsError(email)
+
+            if not self._verifier.verify(password.get_secret_value()):
+                raise WeakPasswordError
+
+            hashed_password = self._hasher.hash_password(password.get_secret_value())
             user = User(
                 email=email,
-                password=password,
+                password=SecretValue(hashed_password),
                 role=UserRole.USER,
             )
             await uow.users.save(user)
