5d554bf87270531d9ecde7bd1ba27404c27398bb
g.kharitonov <haritonovgleb123@gmail.com>
2025-04-05 01:37:38 +0300

    feat: adjust register endpoint, add login and me

--- bases/open_gallery/api/identity/router.py
+++ bases/open_gallery/api/identity/router.py
@@ -1,14 +1,17 @@
 from typing import Annotated
 
 from dishka.integrations.fastapi import FromDishka, inject
-from fastapi import Body
+from fastapi import Body, Depends
 
 from open_gallery.api.identity.schemas import RegisterRequestSchema
+from open_gallery.identity.dtos import TokensPair
 from open_gallery.identity.entities import User
 from open_gallery.identity.exceptions import UserExistsError, WeakPasswordError
+from open_gallery.identity.use_cases.login_user import LoginUserUsecase
 from open_gallery.identity.use_cases.register_user import RegisterUserUsecase
 from open_gallery.routing.logging_route import LoggingRoute
 from open_gallery.routing.router import APIRouter
+from open_gallery.shared_api.authentication.security import authorized
 from open_gallery.shared_api.exceptions import define_possible_errors
 
 identity_router = APIRouter(prefix="/identity", route_class=LoggingRoute)
@@ -32,5 +35,22 @@ async def test() -> str:
 async def register_endpoint(
     request_body: Annotated[RegisterRequestSchema, Body()],
     register: FromDishka[RegisterUserUsecase],
-) -> User:
+) -> TokensPair:
     return await register(email=request_body.email, password=request_body.password)
+
+
+@identity_router.post(
+    "/login",
+)
+@inject
+async def login_endpoint(
+    request_body: Annotated[RegisterRequestSchema, Body()],
+    login: FromDishka[LoginUserUsecase],
+) -> TokensPair:
+    return await login(email=request_body.email, password=request_body.password)
+
+
+@identity_router.post("/me")
+@inject
+async def show_me_endpoint(user: Annotated[User, Depends(authorized)]) -> User:
+    return user
