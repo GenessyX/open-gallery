7377fd630425ea640f955cec5402e1884dc8e4ca
g.kharitonov <haritonovgleb123@gmail.com>
2025-04-05 01:35:38 +0300

    feat: handle auth errors in api

--- bases/open_gallery/api/identity/exceptions.py
+++ bases/open_gallery/api/identity/exceptions.py
@@ -2,7 +2,13 @@ from http import HTTPStatus
 
 from fastapi import Request
 
-from open_gallery.identity.exceptions import UserError, UserExistsError, WeakPasswordError
+from open_gallery.identity.exceptions import (
+    AuthorizationError,
+    InvalidCredentialsError,
+    UserError,
+    UserExistsError,
+    WeakPasswordError,
+)
 from open_gallery.shared_api.exceptions import APIError, to_exception_handler
 
 
@@ -13,6 +19,8 @@ def user_error_handler(_: Request, exception: UserError) -> APIError:
             status_code = HTTPStatus.CONFLICT
         case WeakPasswordError():
             status_code = HTTPStatus.BAD_REQUEST
+        case AuthorizationError() | InvalidCredentialsError():
+            status_code = HTTPStatus.UNAUTHORIZED
         case _:
             status_code = HTTPStatus.INTERNAL_SERVER_ERROR
 
