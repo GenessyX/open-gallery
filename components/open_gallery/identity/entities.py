dd1068d0ab5cf4a4d007b6062f57b46dba7434ef
g.kharitonov <haritonovgleb123@gmail.com>
2025-04-05 10:50:33 +0300

    feat: store refresh tokens in user's context

--- components/open_gallery/identity/entities.py
+++ components/open_gallery/identity/entities.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from enum import Enum
 from typing import NewType
 
-from open_gallery.shared.entity import Entity, EntityId
+from open_gallery.shared.entity import Entity, EntityId, SubEntity
 from open_gallery.shared.types import Email, SecretValue
 
 UserId = NewType("UserId", EntityId)
@@ -14,8 +14,15 @@ class UserRole(str, Enum):
 
 
 @dataclass(kw_only=True)
+class RefreshToken(SubEntity):
+    token_hash: SecretValue[str]
+
+
+@dataclass(kw_only=True)
 class User(Entity):
     id: UserId = field(default_factory=lambda: UserId(EntityId.generate()))  # pyright: ignore[reportIncompatibleVariableOverride]
     email: Email
     password: SecretValue[str]
     role: UserRole
+    refresh_tokens: list[RefreshToken] = field(default_factory=list)
+    verified: bool = False
