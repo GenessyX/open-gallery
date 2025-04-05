e8e6b7aacb9107895b9cbc9c4c451e9afe583ad0
g.kharitonov <haritonovgleb123@gmail.com>
2025-04-05 10:50:50 +0300

    feat: set up refresh tokens table

--- components/open_gallery/persistence/tables/users.py
+++ components/open_gallery/persistence/tables/users.py
@@ -1,9 +1,10 @@
 from sqlalchemy import String, Table, func
-from sqlalchemy.orm import registry
+from sqlalchemy.orm import registry, relationship
 
-from open_gallery.identity.entities import User
+from open_gallery.identity.entities import RefreshToken, User
 from open_gallery.persistence.tables.base import Column, datetime_columns, mapper_registry
-from open_gallery.persistence.tables.keys import UserPrimaryKeyType
+from open_gallery.persistence.tables.keys import UserForeignKey, UserPrimaryKeyType
+from open_gallery.persistence.type_decorators.datetime import UTCDateTime
 from open_gallery.persistence.type_decorators.identity import UserRoleTypeImpl
 from open_gallery.persistence.type_decorators.shared_types import SecretValueTypeImpl
 
@@ -17,9 +18,28 @@ users = Table(
     Column("role", UserRoleTypeImpl),
 )
 
+refresh_tokens = Table(
+    "refresh_tokens",
+    mapper_registry.metadata,
+    Column("user_id", UserPrimaryKeyType, UserForeignKey(), primary_key=True),
+    Column("token_hash", SecretValueTypeImpl, primary_key=True),
+    Column("created_at", UTCDateTime, server_default=func.now()),
+)
+
 
 def bind_mappers(mapper_registry: registry) -> None:
     mapper_registry.map_imperatively(
+        RefreshToken,
+        refresh_tokens,
+    )
+    mapper_registry.map_imperatively(
         User,
         users,
+        properties={
+            "refresh_tokens": relationship(
+                RefreshToken,
+                uselist=True,
+                lazy="selectin",
+            ),
+        },
     )
