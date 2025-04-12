7b27d79884d75405c43fdd1bc350f5e1b143fe2d
g.kharitonov <haritonovgleb123@gmail.com>
2025-04-12 14:55:07 +0300

    perf: use data from jwt to construct user entity
    Avoid loading user from database if we have access-token in JWT

--- components/open_gallery/shared_api/authentication/ioc.py
+++ components/open_gallery/shared_api/authentication/ioc.py
@@ -1,12 +1,15 @@
 from dishka import FromDishka, Provider, Scope, provide
 from fastapi import Request
+from sqlalchemy import inspect
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import make_transient_to_detached
 
 from open_gallery.identity.dtos import AccessTokenPayload
 from open_gallery.identity.entities import User, UserId, UserRole
 from open_gallery.identity.exceptions import AuthorizationError
-from open_gallery.identity.repository import UserRepository
 from open_gallery.jwt.interface import JWTService
 from open_gallery.shared.entity import EntityId
+from open_gallery.shared.types import SecretValue
 from open_gallery.shared_api.authentication.security import security
 
 
@@ -33,13 +36,23 @@ class AuthorizationProvider(Provider):
         self,
         request: Request,
         jwt_service: FromDishka[JWTService[AccessTokenPayload]],
-        user_repository: FromDishka[UserRepository],
+        session: FromDishka[AsyncSession],
     ) -> User:
         credentials = await security(request)
         if not credentials:
             raise AuthorizationError
         decoded_token = jwt_service.decode(credentials.credentials)
-        user = await user_repository.get(entity_id=UserId(EntityId(decoded_token.sub)))
-        if not user:
-            raise AuthorizationError
+
+        user = User(
+            id=UserId(EntityId(decoded_token.sub)),
+            email=decoded_token.email,
+            password=SecretValue(""),
+            role=decoded_token.role,
+        )
+
+        make_transient_to_detached(user)
+        await session.merge(user, load=False)
+
+        session.identity_map.add(inspect(user))  # type: ignore[arg-type]
+
         return user
